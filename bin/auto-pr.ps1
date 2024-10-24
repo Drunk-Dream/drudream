24e2a19d1abec58633dc08de55a4429d491d3341
Drunk-Dream <100981449+Drunk-Dream@users.noreply.github.com>
2024-10-24 13:28:21 +0800

    Update auto-pr.ps1
    Replace the placeholder repository string in bin/auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "Drunk-Dream/drudream:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
